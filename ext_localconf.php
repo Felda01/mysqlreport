91e8fc1cbdb20e7b63b7c301d0f5abff484c44b7
Stefan Froemken <froemken@gmail.com>
2017-01-23 11:43:09 +0100

    Login works again, if ExtConf BE and FE are disabled

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,14 +3,13 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\DatabaseConnection']['className'] = 'StefanFroemken\\Mysqlreport\\Database\\DatabaseConnection';
-
 if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
     $extConf = is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]) ?: unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
     if (
         ($extConf['profileFrontend'] && TYPO3_MODE === 'FE') ||
         ($extConf['profileBackend'] && TYPO3_MODE === 'BE')
     ) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\DatabaseConnection']['className'] = 'StefanFroemken\\Mysqlreport\\Database\\DatabaseConnection';
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['setDBinit'] .= LF . ' SET profiling = 1;';
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']['queryProcessors'][] = 'StefanFroemken\\Mysqlreport\\Database\\DatabaseHooks';
     }
