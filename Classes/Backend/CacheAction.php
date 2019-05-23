9d980b9c05609f527f17ec853f69a30734831646
Stefan Froemken <froemken@gmail.com>
2019-05-23 22:58:09 +0200

    Update ClearCache Button

--- Classes/Backend/CacheAction.php
+++ Classes/Backend/CacheAction.php
@@ -14,9 +14,8 @@ namespace StefanFroemken\Mysqlreport\Backend;
  * The TYPO3 project - inspiring people to share!
  */
 use TYPO3\CMS\Backend\Toolbar\ClearCacheActionsHookInterface;
-use TYPO3\CMS\Backend\Utility\IconUtility;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
@@ -25,70 +24,34 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class CacheAction implements ClearCacheActionsHookInterface
 {
     /**
-     * @var \TYPO3\CMS\Core\Database\DatabaseConnection
-     */
-    protected $databaseConnection;
-
-    /**
-     * constructor of this class
-     */
-    public function __construct()
-    {
-        $this->databaseConnection = $GLOBALS['TYPO3_DB'];
-    }
-
-    /**
      * Modifies CacheMenuItems array
      *
      * @param array $cacheActions Array of CacheMenuItems
      * @param array $optionValues Array of AccessConfigurations-identifiers (typically used by userTS with options.clearCache.identifier)
-     *
-     * @return void
      */
     public function manipulateCacheActions(&$cacheActions, &$optionValues)
     {
-        $cacheAction = [
-            'id' => 'mysqlprofiles',
-            'title' => 'Clear MySQL Profiles',
-            'description' => 'Clear collected profile records of extension mysqlreport. This table can grow very fast, so maybe it is good to clear this table.',
+        $cacheActions[] = [
+            'id' => 'mysqlreport',
+            'title' => 'LLL:EXT:mysqlreport/Resources/Private/Language/locallang.xlf:module.clearCache.title',
+            'description' => 'LLL:EXT:mysqlreport/Resources/Private/Language/locallang.xlf:module.clearCache.description',
+            'href' => BackendUtility::getModuleUrl('tce_db', ['cacheCmd' => 'mysqlreport']),
+            'iconIdentifier' => 'actions-system-cache-clear-impact-high'
         ];
-        if (GeneralUtility::compat_version('7.6')) {
-            /** @var IconFactory $iconFactory */
-            $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
-            $cacheAction['icon'] = $iconFactory->getIcon('actions-system-cache-clear-impact-high');
-            $cacheAction['href'] = BackendUtility::getModuleUrl('tce_db', [
-                'vC' => $this->getBackendUser()->veriCode(),
-                'cacheCmd' => 'mysqlprofiles',
-                'ajaxCall' => 1
-            ]);
-        } else {
-            $cacheAction['icon'] = IconUtility::getSpriteIcon('actions-system-cache-clear-impact-high');
-            $cacheAction['href'] = 'tce_db.php?vC=' . $this->getBackendUser()->veriCode() . '&cacheCmd=mysqlprofiles&ajaxCall=1' . BackendUtility::getUrlToken('tceAction');
-        }
-        $cacheActions[] = $cacheAction;
-        $optionValues[] = 'mysqlprofiles';
+        $optionValues[] = 'mysqlreport';
     }
 
     /**
-     * Returns the current BE user.
-     *
-     * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
-     */
-    protected function getBackendUser()
-    {
-        return $GLOBALS['BE_USER'];
-    }
-
-    /**
-     * truncate table tx_mysqlreport_domain_model_profile
+     * Truncate table tx_mysqlreport_domain_model_profile
      *
      * @param array $params
-     * @return void
      */
-    public function clearProfiles($params = [])
+    public function clearMySqlReports($params = [])
     {
-        if ($params['cacheCmd'] === 'mysqlprofiles') {
-            $this->databaseConnection->exec_TRUNCATEquery('tx_mysqlreport_domain_model_profile');
+        if ($params['cacheCmd'] === 'mysqlreport') {
+            $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+            $connection = $connectionPool->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME);
+            $connection->truncate('tx_mysqlreport_domain_model_profile');
         }
     }
 }
