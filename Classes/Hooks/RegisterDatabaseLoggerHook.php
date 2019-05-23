59939cbb98796e17388e8365585595b8fedf23b9
Stefan Froemken <froemken@gmail.com>
2019-05-23 23:01:43 +0200

    Add DebugLogger to Doctrine and log Queries to DB

---
+++ Classes/Hooks/RegisterDatabaseLoggerHook.php
@@ -0,0 +1,84 @@
+<?php
+namespace StefanFroemken\Mysqlreport\Hooks;
+
+/*
+ * This file is part of the mysqlreport project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Doctrine\DBAL\Logging\DebugStack;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Add Logger to database connection
+ */
+class RegisterDatabaseLoggerHook implements SingletonInterface
+{
+    public function addLoggerToDatabaseConnection()
+    {
+        $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+        // @ToDo: Loop through all Connection names
+        $connection = $connectionPool->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME);
+        $connection->getConfiguration()->setSQLLogger(
+            GeneralUtility::makeInstance(DebugStack::class)
+        );
+    }
+
+    /**
+     * destructor of this class
+     */
+    public function __destruct()
+    {
+        $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+        $connection = $connectionPool->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME);
+
+        // do not log our insert queries
+        $sqlLogger = clone $connection->getConfiguration()->getSQLLogger();
+        $connection->getConfiguration()->setSQLLogger(null);
+
+        // A page can be called multiple times each second. So we need an unique identifier.
+        $uniqueIdentifier = uniqid();
+        $pid = is_object($GLOBALS['TSFE']) ? $GLOBALS['TSFE']->id : 0;
+
+        if ($sqlLogger instanceof DebugStack) {
+            $queriesToStore = [];
+            foreach ($sqlLogger->queries as $key => $loggedQuery) {
+                $queriesToStore[] = [
+                    'pid' => $pid,
+                    'query_type' => GeneralUtility::trimExplode(' ', $loggedQuery['sql'], true, 2)[0],
+                    'duration' => $loggedQuery['executionMS'],
+                    'query' => $connection->quote($loggedQuery['sql']),
+                    'profile' => serialize([]),
+                    'explain_query' => serialize([]),
+                    'not_using_index' => 0,
+                    'using_fulltable' => 0,
+                    'mode' => (string)TYPO3_MODE,
+                    'unique_call_identifier' => $uniqueIdentifier,
+                    'crdate' => (int)$GLOBALS['EXEC_TIME'],
+                    'query_id' => $key
+                ];
+            }
+
+            /*$connection->bulkInsert(
+                'tx_mysqlreport_domain_model_profile',
+                $queriesToStore
+            );*/
+            foreach ($queriesToStore as $query) {
+                $connection->insert(
+                    'tx_mysqlreport_domain_model_profile',
+                    $query
+                );
+            }
+        }
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,20 +3,9 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
-    $extConf = is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]) ?: unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-    if (
-        ($extConf['profileFrontend'] && TYPO3_MODE === 'FE') ||
-        ($extConf['profileBackend'] && TYPO3_MODE === 'BE')
-    ) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\DatabaseConnection']['className'] = 'StefanFroemken\\Mysqlreport\\Database\\DatabaseConnection';
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['setDBinit'] .= LF . ' SET profiling = 1;';
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']['queryProcessors'][] = 'StefanFroemken\\Mysqlreport\\Database\\DatabaseHooks';
-    }
-}
+// Add Debug Logger to Doctrine via first Hook in TYPO3
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest'][] = \StefanFroemken\Mysqlreport\Hooks\RegisterDatabaseLoggerHook::class . '->addLoggerToDatabaseConnection';
 
-if (TYPO3_MODE === 'BE') {
-    // add button to clear cache of profiling table
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][] = 'StefanFroemken\\Mysqlreport\\Backend\\CacheAction';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'StefanFroemken\\Mysqlreport\\Backend\\CacheAction->clearProfiles';
-}
+// add button to clear cache of profiling table
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][] = \StefanFroemken\Mysqlreport\Backend\CacheAction::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = \StefanFroemken\Mysqlreport\Backend\CacheAction::class . '->clearMySqlReports';
